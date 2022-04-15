ae07cbebd7d50c991b1669d2f8c10327f45c0d96
denizbabayaka@gmail.com <denizbabayaka@gmail.com>
2022-04-15 14:18:19 -0400

    filees added

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,12 +1,15 @@
-const {test}=require('@playwright/test'); // import {test} from '@playwright/test';
+const { test } = require('@playwright/test'); // import {test} from '@playwright/test';
 
 // test annotation is used to describe a test case.coming from @playwright/test
-test('First Playwright test',  function()
-{ 
-  const browser = await t.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.goto('https://www.google.com');
-  await page.screenshot({path: 'google.png'});
-  await browser.close();
+// browser is a global variable that is available in all test cases.
+test('First Playwright test', async ({browser}) =>
+// Since javascript asyncronus we have to write await command to exucute the code in order 
+{
+   //Creates a new browser instance context. It won't share cookies/cache with other browser contexts.
+   const context = browser.newContext();
+   // Creates a new page in the browser context.
+    context.newPage();  
+   
+   
+
 });
