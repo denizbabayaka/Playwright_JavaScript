e417eae0213dacf971786365f5d38ea4403d7bff
denizbabayaka@gmail.com <denizbabayaka@gmail.com>
2022-05-02 00:35:23 -0400

    new files added

--- tests/WebAPIpart2.spec.js
+++ tests/WebAPIpart2.spec.js
@@ -7,6 +7,7 @@ const { test, expect, request } = require('@playwright/test'); // import {test} 
 //This is a javaScript object so no need to put double qoute to the key 
 const loginPayLoad = { userEmail: "anshika@gmail.com", userPassword: "Iamking@000" }
 const orderPayLoad = { orders: [{ country: "Cuba", productOrderedId: "620c7dee48767f1f1215d2de" }] };
+//in global (class) variable we use let keyword for later initalization
 let token = null;
 let orderId = null;
 
@@ -64,82 +65,9 @@ test('Place the order', async ({ browser, page }) => {
     page.addInitScript(value => {
         window.localStorage.setItem('token', value);
     }, token);
+    //uP TO THIS STEP EVER DATA IS FEEDED TO THE APP BY API POST REQUEST WE ARE ONLY
+    //VALIDATING THE ORDER ID FROM NOW ON
     await page.goto("https://rahulshettyacademy.com/client")
-    // await page.locator("#userEmail").fill(email);
-    // await page.locator("#userPassword").type("Iamking@000");
-    // await page.locator("[value='Login']").click(); 
-    //\const productName = 'zara coat 3';
-    const email = "anshika@gmail.com";
-    const products = page.locator(".card-body");
-
-    //Since html works on API calls this code will wait all elements  until all the get calls are done
-    //and page fully loaded and DOM is ready basically this code mekes an API call and waits for the response
-    // if we use that code we do not need to use line 21 
-    //Those method works for service based web application which allow API call to the other 
-    //third parties
-    //\await page.waitForLoadState("networkidle");
-    //\await page.locator(".card-body b").nth(0).textContent();
-    //\const allTextContents = await page.locator(".card-body b").allTextContents();
-    //\console.log(allTextContents);
-    //\const count = await products.count();
-    //\for (let i = 0; i < count; i++) {
-    //This will reach the b tag which has got the text of this element in the same div tag
-    // and extract the text from that tag
-    //\if (await products.nth(i).locator("b").textContent() === productName) {
-    // This will find the element which has got the text of this element in the same div tag
-    //this tecnique we call locator chain
-    //\await products.nth(i).locator("text= Add To Cart").click();
-    //\break;
-    //\}
-
-    //\}
-
-
-
-    //\await page.locator("[routerlink*='cart']").click();
-
-    //This locator will wait until the first element is visible on the page for loading
-    //\await page.locator("div li").last().waitFor();
-
-    //since there could be more than one "Zara Coat 4" text in other div tags
-    //we are giving the specific tag of that method to get the text
-    //Those locating methods belong to Playwright API
-    //\const bool = await page.locator("h3:has-text('zara coat 3')").isVisible();
-    //\expect(bool).toBeTruthy();
-
-    //Those are 2 ways to get the text locator of the checkout button
-    //await page.locator("button:has-text('Checkout')").click();
-    //\await page.locator('button[type="button"]').nth(1).click();
-    //This delay method will type the chracters in the text box in order to synchronize with the
-    //text box otherwise page can not catch the playwright 
-    //\await page.locator('[placeholder="Select Country"]').type("ind", { delay: 100 });
-    // we will get all the results from dropdown
-    //\const dropdown = page.locator('.ta-results')
-    // wait for the dropdown to be visible
-    //\await dropdown.waitFor();
-    // get the count index of the dropdown options 
-    //\const optionsCount = await dropdown.locator("button").count();
-    //loop through the options
-    //\for (let i = 0; i < optionsCount; ++i) {
-    // get the text of the options based on index
-    //\text = await dropdown.locator("button").nth(i).textContent();
-    //Inside DOM this element text has got a space be aware of that
-    //\if (text === " India") {
-    //\await dropdown.locator("button").nth(i).click();
-    //\break;
-    //\}
-    //\}
-
-
-
-    //.user_name [type='text'] => chaining css locator
-    //\await expect(page.locator("label[type='text']")).toHaveText(email);
-    //\await page.locator('.action__submit').click();
-
-    //\await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. "); //
-     //\const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
-     //\console.log(orderId);
-
     await page.locator('text="ORDERS"').click();
     await page.locator("tbody").waitFor();
 
@@ -149,6 +77,7 @@ test('Place the order', async ({ browser, page }) => {
     for (let i = 0; i < await rows.count(); ++i) {
         // get the textContent of the inside the row
         const roworderId = await rows.nth(i).locator("th").textContent();
+        //This order id is coming from the API post  (line 44) call 
         // we are using include method to check if the order id is present in the row
         if (orderId.includes(roworderId)) {
             await rows.nth(i).locator("button").first().click();
@@ -172,4 +101,6 @@ test('Place the order', async ({ browser, page }) => {
 
 });
 
-//npx playwright test tests/ClientApp.spec.js =>run specific file in the folder
+
+
+    
